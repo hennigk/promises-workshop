b675d07baaed1a521b7f21069d80c9b8aaf207da
hennigk <kayla.hennig@gmail.com>
2015-09-24 20:39:59 +0000

    exercise 2

---
+++ ex-2.js
@@ -0,0 +1,64 @@
+var Promise = require("bluebird");
+
+
+function delay(milliseconds) {
+    return new Promise(function(resolve) {
+    setTimeout(function() {
+        resolve();
+    }, milliseconds);
+});
+}
+
+function getFirstChar(firstChar) {
+    return delay(500).then(
+        function() {
+            return firstChar[0];
+    } );
+}
+
+
+function getLastChar(lastChar){
+    return delay(500).then(
+        function() {
+            return lastChar[lastChar.length -1];
+    });
+}
+
+
+function getFirstAndLastCharSeq(firstLastChar){
+    var firstChar;
+    return getFirstChar(firstLastChar).then(
+        function(gotFirstChar){
+            firstChar = gotFirstChar;
+            return getLastChar(firstLastChar);
+            }).then(
+                function(lastChar){
+                    return firstChar + lastChar;
+                });
+        }
+
+
+function getFirstAndLastCharParallel(parallelString){
+    return Promise.join(getFirstChar(parallelString), getLastChar(parallelString), function(firstChar, lastChar) {
+        return firstChar + lastChar;
+    });
+}
+
+
+getLastChar("Kayla").then(
+    function(lastChar){
+        console.log(lastChar);
+});
+getFirstChar("Kayla").then(
+    function(firstChar){
+        console.log(firstChar);
+});
+getFirstAndLastCharSeq("Kayla").then(
+    function(firstLastChar){
+        console.log(firstLastChar);
+});
+
+getFirstAndLastCharParallel("Kayla").then(
+    function(firstLastChar){
+        console.log(firstLastChar);
+});
