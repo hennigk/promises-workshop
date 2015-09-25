142e9b31a3de0bc2a9d968e7178e54d7ff6d06a9
hennigk <kayla.hennig@gmail.com>
2015-09-25 14:36:00 +0000

    formtted functions and promises

--- ex-2.js
+++ ex-2.js
@@ -3,24 +3,24 @@ var Promise = require("bluebird");
 
 function delay(milliseconds) {
     return new Promise(function(resolve) {
-    setTimeout(function() {
-        resolve();
-    }, milliseconds);
-});
+        setTimeout(function() {
+            resolve();
+        }, milliseconds);
+    });
 }
 
 function getFirstChar(firstChar) {
     return delay(500).then(
-        function() {
-            return firstChar[0];
+    function() {
+        return firstChar[0];
     } );
 }
 
 
 function getLastChar(lastChar){
     return delay(500).then(
-        function() {
-            return lastChar[lastChar.length -1];
+    function() {
+        return lastChar[lastChar.length -1];
     });
 }
 
@@ -28,37 +28,40 @@ function getLastChar(lastChar){
 function getFirstAndLastCharSeq(firstLastChar){
     var firstChar;
     return getFirstChar(firstLastChar).then(
-        function(gotFirstChar){
-            firstChar = gotFirstChar;
-            return getLastChar(firstLastChar);
-            }).then(
-                function(lastChar){
-                    return firstChar + lastChar;
-                });
-        }
+    function(gotFirstChar){
+        firstChar = gotFirstChar;
+        return getLastChar(firstLastChar);
+    }).then(
+    function(lastChar){
+        return firstChar + lastChar;
+    });
+}
 
 
 function getFirstAndLastCharParallel(parallelString){
-    return Promise.join(getFirstChar(parallelString), getLastChar(parallelString), function(firstChar, lastChar) {
+    return Promise.join(getFirstChar(parallelString), getLastChar(parallelString), 
+    function(firstChar, lastChar) {
         return firstChar + lastChar;
     });
 }
 
 
 getLastChar("Kayla").then(
-    function(lastChar){
-        console.log(lastChar);
-});
+function(lastChar){
+    console.log(lastChar);
+    });
+
 getFirstChar("Kayla").then(
-    function(firstChar){
-        console.log(firstChar);
-});
+function(firstChar){
+    console.log(firstChar);
+    });
+
 getFirstAndLastCharSeq("Kayla").then(
-    function(firstLastChar){
-        console.log(firstLastChar);
-});
+function(firstLastChar){
+    console.log(firstLastChar);
+    });
 
 getFirstAndLastCharParallel("Kayla").then(
-    function(firstLastChar){
-        console.log(firstLastChar);
+function(firstLastChar){
+    console.log(firstLastChar);
 });
